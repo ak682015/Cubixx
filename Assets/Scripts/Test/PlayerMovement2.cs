3f30f92d37bbe687e9d2fb22ba7b2f4a963b6773
sahil <arman.khan@projectasfalis.in>
2020-04-05 20:51:22 +0530

    Minor Changes in PlayeScript

--- Assets/Scripts/Test/PlayerMovement2.cs
+++ Assets/Scripts/Test/PlayerMovement2.cs
@@ -3,23 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement2 : MonoBehaviour
 {
 
     public Rigidbody rb;
     public float forwardForce = 2000f;
-    public float sidewaysForce = 200f;
+    public float sidewaysForce = 300f;
 
     private Gyroscope gyro;
     private bool gyroEnabled;
     private Quaternion rot;
-        
 
+    private float ScreenWidth;
+
+
+    private void Start()
+    {
+        ScreenWidth = Screen.width;
+
+
+    }
+
+    /*
     private void Start()
     {
         gyroEnabled = EnableGyro();
     }
-
+   
     private bool EnableGyro()
     {
         if (SystemInfo.supportsGyroscope)
@@ -30,29 +40,50 @@ public class PlayerMovement : MonoBehaviour
         }
         return false;
     }
+        */
 
     void FixedUpdate()
     {
 
-
-
         forwardForce += 0.5f;
-        forwardForce = Mathf.Clamp(forwardForce, 2000f, 4000f);
+        forwardForce = Mathf.Clamp(forwardForce, 2000f, 4000f); 
 
-        if (transform.position.y < -3)
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+
+        if( transform.position.y < -3 )
         {
             Invoke("Restart", 1f);
         }
-
-        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
-       
-        if(gyroEnabled)
+        
+        /*
+        if (gyroEnabled)
         {
-            rb.AddForce(- sidewaysForce * gyro.attitude.x * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+            rb.AddForce(-sidewaysForce * gyro.attitude.x * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
 
         }
+        */
+
+        if (Input.touchCount > 0)
+        {
+           
+            Touch touch = Input.GetTouch(0);
+            if(touch.position.x > ScreenWidth / 2)
+            {
+                rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+
+            }
+
+            if (touch.position.x < ScreenWidth / 2)
+            {
+                rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+
+            }
+            
+        }
+
         
-        if ( Input.GetKey("d"))
+        if (Input.GetKey("d"))
         {
             rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
