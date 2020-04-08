f33c8fad42173433e96259423c8a40614a5fdcfc
cshoaib05 <choudharyshoaib05@gmail.com>
2020-04-08 20:28:26 +0530

    color changing done

---
+++ Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+    public Camera camera;
+    public Material obstacleMaterial;
+
+    public Color[] bgColor;
+    public Color[] obstacleColor;
+   
+    void Start()
+    {
+        changeColor();
+    }
+
+    void changeColor()
+    {
+        int i;
+        i = Random.Range(0, bgColor.Length);
+        camera.backgroundColor = bgColor[i];
+        obstacleMaterial.SetColor("_BaseColor",obstacleColor[i]);
+    }
+}
